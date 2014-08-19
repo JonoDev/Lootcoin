52d7101fa8db690fbb6c94c7885726b5d3557a26
JonoDev <Jonathon@Jonathons-MacBook-Pro.local>
2014-08-19 03:25:31 +0100

    The birth of Lootcoin

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("BAR");
-    case mBTC: return QString("mBAR");
-    case uBTC: return QString::fromUtf8("μBAR");
+    case BTC: return QString("LOC");
+    case mBTC: return QString("mLOC");
+    case uBTC: return QString::fromUtf8("μLOC");
     default: return QString("???");
     }
 }
@@ -45,13 +45,13 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("barcoin");
-    case mBTC: return QString("millibarcoin (1 / 1,000)");
-    case uBTC: return QString("microbarcoin (1 / 1,000,000)");
+    case BTC: return QString("lootcoin");
+    case mBTC: return QString("millilootcoin (1 / 1,000)");
+    case uBTC: return QString("microlootcoin (1 / 1,000,000)");
     default: return QString("???");
     }
 }
-//a single unit (.00000001) of barcoin is called a "wander."
+//a single unit (.00000001) of lootcoin is called a "wander."
 qint64 BitcoinUnits::factor(int unit)
 {
     switch(unit)
